56810b125b356a1cd595a69387a825f40994c0c4
ediaz <alcmaeon@gmail.com>
2017-11-13 17:01:54 -0800

    Fixed typo in function of app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+'use strict';
+
 var express = require('express'),
     app = express();
 
 app.set('views', 'views');
 app.set('view engine', 'jade');
 
-app.get('/', function () {
+app.get('/', function (req, res) {
     res.render('home', {
     });
 });
